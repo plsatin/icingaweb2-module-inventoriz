94a9b80f607d11399c3a3f75fe1ec64886a59e6c
Павел Сатин <it4@rezhcable.ru>
2022-11-18 09:49:25 +0500

    headers

--- public/proxy.php
+++ public/proxy.php
@@ -5,26 +5,31 @@ $baseURL = $_REQUEST['url'];
 
 // getting the request method sent in to the proxy
 function getRequestMethod() {
-  return $_SERVER["REQUEST_METHOD"]; 
+    return $_SERVER["REQUEST_METHOD"]; 
 }
 
 // getting the POST data from a POST request
 function getPostData() {
-  return http_build_query($_POST);
+    return http_build_query($_POST);
 }
 
+$headers = getallheaders();  
 
-function makeGetRequest($baseURL) {
-  $ch = curl_init();
-  $fullURL = $baseURL; 
 
-  curl_setopt($ch, CURLOPT_URL, $fullURL);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 
-  $response = curl_exec($ch);
-  curl_close($ch);
+function makeGetRequest($baseURL, $headers) {
+    $ch = curl_init();
+    $fullURL = $baseURL; 
+
+    curl_setopt($ch, CURLOPT_URL, $fullURL);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
   
     if($e = curl_error($ch)) {
         echo $e;
@@ -35,19 +40,21 @@ function makeGetRequest($baseURL) {
     }
 }
 
-function makePostRequest($baseURL) {
+function makePostRequest($baseURL, $headers) {
     $ch = curl_init();
-  $data = http_build_query($_POST);
+    $data = http_build_query($_POST);
 
-  curl_setopt($ch, CURLOPT_URL, $baseURL);
+    curl_setopt($ch, CURLOPT_URL, $baseURL);
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-  $response = curl_exec($ch);
-  curl_close($ch);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
 
     if($e = curl_error($ch)) {
         echo $e;
@@ -60,15 +67,15 @@ function makePostRequest($baseURL) {
 
 $response = "";
 switch (getRequestMethod()) {
-  case 'GET':
-    $response = makeGetRequest($baseURL);
-    break;
-  case 'POST':
-    $response = makePostRequest($baseURL);
-    break;
-  default:
-    echo "There has been an error";
-    return;
+    case 'GET':
+        $response = makeGetRequest($baseURL);
+        break;
+    case 'POST':
+        $response = makePostRequest($baseURL);
+        break;
+    default:
+        echo "There has been an error";
+        return;
 }
 
 /*
