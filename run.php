0ebef702e0d66f8cd825d3be2797df21d63c645b
Павел Сатин <it4@rezhcable.ru>
2022-11-24 15:10:09 +0500

    provideHook

--- run.php
+++ run.php
@@ -1,6 +1,12 @@
 <?php
 
-$this->registerHook('Monitoring\\HostActions', '\\Icinga\\Module\\Inventoriz\\HostActions');
-$this->registerHook('Monitoring\\ServiceActions', '\\Icinga\\Module\\Inventoriz\\ServiceActions');
-
+// $this->registerHook('Monitoring\\HostActions', '\\Icinga\\Module\\Inventoriz\\HostActions');
+// $this->registerHook('Monitoring\\ServiceActions', '\\Icinga\\Module\\Inventoriz\\ServiceActions');
 // $this->provideHook('Monitoring\\DetailviewExtension', '\\Icinga\\Module\\Inventoriz\\DetailviewExtension');
+
+
+//// Для версии - Icinga Web 2 Version 2.11.2
+$this->provideHook('Monitoring\\HostActions', '\\Icinga\\Module\\Inventoriz\\HostActions');
+$this->provideHook('Monitoring\\ServiceActions', '\\Icinga\\Module\\Inventoriz\\ServiceActions');
+
+
